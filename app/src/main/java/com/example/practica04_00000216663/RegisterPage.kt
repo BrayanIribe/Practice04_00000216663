792042ad93803438050eb1bb74c2e87797a1bc12
Brayan Iribe <brayaniribe@gmail.com>
2022-02-13 22:54:15 -0700

    Added Register & Menu page

---
+++ app/src/main/java/com/example/practica04_00000216663/RegisterPage.kt
@@ -0,0 +1,20 @@
+package com.example.practica04_00000216663
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.practica04_00000216663.R.id.btnGetStarted
+
+class RegisterPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+
+        val button: Button = findViewById(R.id.button_sign_in) as Button
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, MenuPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
